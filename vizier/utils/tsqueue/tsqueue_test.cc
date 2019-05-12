82ca666520a0a2973d3e2b65fca9a2a81a6bb18a
pglotfel <pglotfel@gmail.com>
2019-05-12 09:27:08 -0400

    Added test for tsqueue

---
+++ vizier/utils/tsqueue/tsqueue_test.cc
@@ -0,0 +1,22 @@
+#include "vizier/utils/tsqueue/tsqueue.h"
+#include "gtest/gtest.h"
+#include <chrono>
+#include <optional>
+
+TEST(Enqueue, Basic) {
+    ThreadSafeQueue<size_t> q;
+    for(size_t i = 0; i < size_t(50); ++i) {
+        q.enqueue(i);
+    }
+
+    for(size_t i = 0; i < size_t(50); ++i) {
+        EXPECT_EQ(i, q.dequeue());
+    }
+}
+
+TEST(Dequeue, Timeout) {
+    ThreadSafeQueue<int> q;
+
+    std::optional<int> result = q.dequeue(std::chrono::milliseconds(1000)); 
+    EXPECT_FALSE(bool(result));
+}
